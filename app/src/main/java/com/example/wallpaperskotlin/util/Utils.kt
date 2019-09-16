d0e1926bf18342fd9e12ac2a05e2375e916e6e51
deniskole <deniskole1@gmail.com>
2019-09-16 12:39:11 +0300

    Dependencies Added

---
+++ app/src/main/java/com/example/wallpaperskotlin/util/Utils.kt
@@ -0,0 +1,30 @@
+package com.example.wallpaperskotlin.util
+
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+
+object Utils {
+    //For API 23+ you need to request the read/write permissions even if they are already in your manifest.
+    private val REQUEST_EXTERNAL_STORAGE = 1
+    private val PERMISSIONS_STORAGE = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+
+    fun verifyStoragePermissions(activity: Activity) {
+        // Check if we have write permission
+        val permission =
+            ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                activity,
+                PERMISSIONS_STORAGE,
+                REQUEST_EXTERNAL_STORAGE
+            )
+        }
+    }
+}
